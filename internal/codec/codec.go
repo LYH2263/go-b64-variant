52cf3f4c36a258d5d0d0a5d8bf5440fe19f85b27
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-15 23:26:40 +0800

    fix: Base64 变体

--- internal/codec/codec.go
+++ internal/codec/codec.go
@@ -4,5 +4,5 @@ package codec
 import "encoding/base64"
 
 func EncodeURL(data []byte) string {
-	return base64.StdEncoding.EncodeToString(data) // BUG
+	return base64.URLEncoding.EncodeToString(data)
 }
